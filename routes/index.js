356e97ca058b2ead29c20b9580dfd6cd223287d8
Victor Rivas <vctr90w@gmail.com>
2014-07-06 13:19:06 -0430

    Files cleaned, node routes created

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/urlGetter', function(req,res){
+	url = req.body.url;
+	command = 'crul' + url;
+	ex = function(command,callback){
+		exec(command,function(error,stdout,stderr){
+			callback(stdout,error,stderr);
+		});
+	}
+	ex(command,function(data,error,serror){
+		if(data!==""){
+			res.send(200);
+		}else{
+			res.send(400);
+		}
+	});
+});
+
 module.exports = router;
