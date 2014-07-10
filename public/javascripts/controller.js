557acc538f4b17087a7badf0e8bd9e94ecbc59b8
Victor Rivas <vctr90w@gmail.com>
2014-07-10 00:31:53 -0430

    targetting error messages

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,12 +2,13 @@ var app = angular.module('usys',[]);
 
 app.service('$validator',function(){
 
-	this.urlC = function(status){
+	this.urlC = function(status,errTarget){
 		if(status === 'ok'){
 			$('.error').hide();
 			return true;
-		}else{
-			$('.error').show();
+		}else{console.log(errTarget);
+			errTarget = errTarget.toString();
+			$('#'+errTarget+'.error').show();
 			return false;
 		}
 	}
@@ -16,18 +17,21 @@ app.service('$validator',function(){
 
 app.controller('urlCTRL',function($scope,$http,$validator){
 	if(!$scope.url){
-		$scope.url = [{text:''}];
+		$scope.url = [{text:'',id:0}];
 	}
 	$scope.validate = function(){
 		$http({method:'POST',url:'/urlGetter',data:{url:$scope.url[$scope.url.length-1].text}})
 				.success(function(data,status,headers,config){
 					console.log('ok');
 					$validator.urlC('ok');
-					$scope.url.push({text:$scope.url.text});
+					$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1});
 				})
 				.error(function(data,status,headers,cnfig){
 					console.log('Something is wrong');
-					$validator.urlC('Not found');
+					if($scope.url[$scope.url.length-1].text !== undefined){
+						$scope.url = $scope.url.slice(0,$scope.url.length-1);
+					}
+					$validator.urlC('Not found',$scope.url[$scope.url.length-1].id);
 				});
 	};
 });
