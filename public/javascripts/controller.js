6ac5425fc040774c41d76e19ccdb66dea5caaad9
Victor Rivas <vctr90w@gmail.com>
2014-07-11 20:30:37 -0430

    Disable and delete functions done

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -17,14 +17,15 @@ app.service('$validator',function(){
 
 app.controller('urlCTRL',function($scope,$http,$validator){
 	if(!$scope.url){
-		$scope.url = [{text:'',id:0}];
+		$scope.url = [{text:'',id:0,del:false,disabled:false}];
 	}
 	$scope.validate = function(){
 		$http({method:'POST',url:'/urlGetter',data:{url:$scope.url[$scope.url.length-1].text}})
 				.success(function(data,status,headers,config){
 					console.log('ok');
 					$validator.urlC('ok');
-					$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1});
+					$scope.url[$scope.url.length-1].disabled = true;
+					$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1,del:false,disabled:false});
 				})
 				.error(function(data,status,headers,cnfig){
 					console.log('Something is wrong');console.log($scope.url[$scope.url.length-1].text);
@@ -34,4 +35,16 @@ app.controller('urlCTRL',function($scope,$http,$validator){
 					$validator.urlC('Not found',$scope.url[$scope.url.length-1].id);
 				});
 	};
+
+	$scope.delet = function(id){
+		angular.forEach($scope.url,function(v,k){
+			if(v.id === id) v.del = true;
+		});
+		var oldUrl = $scope.url;
+		$scope.url = [];
+		angular.forEach(oldUrl,function(v,k){
+			if(!v.del) $scope.url.push(v);
+		});
+	}
+
 });
