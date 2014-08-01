0b9e6221c4ec89c1c1f7ead6aa0d4560e02f83d9
Victor Rivas <vctr90w@gmail.com>
2014-08-01 00:16:04 -0430

    Jade template support done

--- public/javascripts/controllers/url_crtl.js
+++ public/javascripts/controllers/url_crtl.js
@@ -1,43 +1,44 @@
 angular.module('usys.controllers',[])
 	.controller('urlCTRL',['$scope','$validator','$sce','$http','cdnObject',function($scope,$validator,$sce,$http,cdnObject){
 
+	$scope.template = {name:'html',opp:'jade',html:true,jade:false};
+	$scope.template_s = function(){
+		if($scope.template.name === 'html'){
+			$scope.template.name = 'jade';
+			$scope.template.opp = 'html';
+			$scope.template.html = false;
+			$scope.template.jade = true;
+		}else{
+			$scope.template.name = 'html';
+			$scope.template.opp = 'jade';
+			$scope.template.html = true;
+			$scope.template.jade = false;
+		}
+	}
+
+//-------------CLIPBOARD AREA--------------
 	var css_clip = new ZeroClipboard($('button#css-button'));
 	var js_clip = new ZeroClipboard($('button#js-button'));
-
-	$scope.path = {
-		css:'',
-		js:''
-	}
-
-	$scope.pathc = function(){
-		angular.forEach($scope.url,function(v,k){
-			if(v.csst!==null){
-				var first = v.tag.slice(0,44);
-				var second =  v.tag.slice(v.tag.length-4,v.tag.length);
-				var package = v.pkg_name;
-				var path = $scope.path.css;
-				v.tag = '';
-				v.tag = first + path + package + second;
-			}else if(v.jst!==null){
-				var first = v.tag.slice(0,36);
-				var second =  v.tag.slice(v.tag.length-11,v.tag.length);
-				var package = v.pkg_name;
-				var path = $scope.path.js;
-				v.tag = '';
-				v.tag = first + path + package + second;
-			}
-		});
-	}
+	var w_css_clip = new ZeroClipboard($('button#wgetc-button'));
+	var w_js_clip = new ZeroClipboard($('button#wgetj-button'));
 
 	css_clip.on('copy',function(event){
 		var clipboard = event.clipboardData;
 		var css_tag = '';
 		angular.forEach($scope.url,function(v,k){
-			if(v.csst!==null){
-				css_tag+=v.tag;
+			if(v.csst!==false){
+				if($scope.template.name==='html'){
+					css_tag+='\t'+v.tag+'\n';
+				}else{
+					css_tag+='\t'+v.jtag+'\n';
+				}
 			}
 		});
-		css_tag = '<head>'+css_tag+'</head>';
+		if($scope.template.name==='html'){
+			css_tag = '<head>\n'+css_tag+'</head>';
+		}else{
+			css_tag = 'head\n'+css_tag;
+		}
 		clipboard.setData('text/plain',css_tag);
 		alert('Copied!');
 	});
@@ -46,13 +47,56 @@ angular.module('usys.controllers',[])
 		var clipboard = event.clipboardData;
 		var js_tag = '';
 		angular.forEach($scope.url,function(v,k){
-			if(v.jst!==null){
-				js_tag+=v.tag;
+			if(v.jst!==false){
+				if($scope.template.name==='html'){
+					js_tag+=v.tag+'\n';
+				}else{
+					js_tag+=v.jtag+'\n';
+				}
 			}
 		});
 		clipboard.setData('text/plain',js_tag);
 		alert('Copied!');
 	});
+	w_js_clip.on('copy',function(event){
+		alert('Copied!');
+	});
+	w_css_clip.on('copy',function(event){
+		alert('Copied!');
+	});
+
+
+
+//----------------------------------------
+
+	$scope.path = {
+		css:'',
+		js:''
+	}
+
+	var tags = function(v,n_first,n_second,fn,pointer){
+		var first = v[pointer].slice(0,n_first);
+		var second =  v[pointer].slice(v[pointer].length-n_second,v[pointer].length);
+		var package = v.pkg_name;
+		var path = $scope.path[fn];
+		v[pointer] = '';
+		v[pointer] = first + path + package + second;
+		return v[pointer];
+	}
+
+	$scope.pathc = function(){
+		angular.forEach($scope.url,function(v,k){
+			if(v.csst!==false){
+				v.tag = tags(v,44,4,'css','tag');
+				v.jtag = tags(v,43,2,'css','jtag');
+			}else if(v.jst!==false){
+				v.tag = tags(v,36,11,'js','tag');
+				v.jtag = tags(v,35,2,'js','jtag');
+			}
+		});
+	}
+
+
 
 	$scope.tagBuilder = function(){
 		var jsT = {
@@ -63,11 +107,19 @@ angular.module('usys.controllers',[])
 			first : '<link type="text/css" rel="stylesheet" src="' + $scope.path.css,
 			second:	'" />'
 		};
-		return {js:jsT,css:cssT};
+		var jjsT = {
+			first : 'script(type="text/javascript",src="' + $scope.path.js,
+			second:	'")'
+		}
+		var jcssT = {
+			first : 'link(type="text/css",rel="stylesheet",src="' + $scope.path.css,
+			second:	'")'
+		}
+		return {js:jsT,css:cssT,jjs:jjsT,jcss:jcssT};
 	}
 
 	if(!$scope.url){
-		$scope.url = [{text:'',id:0,del:false,disabled:false,type:'',tag:'',blank:true,jst:null,csst:null,pkg_name:''}];
+		$scope.url = [{text:'',id:0,del:false,disabled:false,type:'',tag:'',jtag:'',blank:true,jst:false,csst:false,pkg_name:''}];
 	}
 
 	$scope.introduce = function(){
@@ -97,16 +149,20 @@ angular.module('usys.controllers',[])
 			$scope.url[$scope.url.length-1].pkg_name = name[1];
 			if(valid[0] === ".js"){
 				jsT = $scope.tagBuilder();
+				jade_js = jsT.jjs;
 				jsT = jsT.js;
 				$scope.url[$scope.url.length-1].tag = jsT.first + valid[1] + jsT.second;
+				$scope.url[$scope.url.length-1].jtag = jade_js.first + valid[1] + jade_js.second;
 				$scope.url[$scope.url.length-1].jst = true;
 			}else{
 				cssT = $scope.tagBuilder();
+				jade_css = cssT.jcss;
 				cssT = cssT.css;
 				$scope.url[$scope.url.length-1].tag = cssT.first + valid[1] + cssT.second;
+				$scope.url[$scope.url.length-1].jtag = jade_css.first + valid[1] + jade_css.second;
 				$scope.url[$scope.url.length-1].csst = true;
 			}
-			$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1,del:false,disabled:false,blank:true,csst:null,jst:null,pkg_name:''});
+			$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1,del:false,disabled:false,blank:true,csst:false,jst:false,pkg_name:'',tag:'',jtag:''});
 		}
 	}
 
