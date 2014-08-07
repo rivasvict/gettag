1270cf143e54c9b787a12317d1ae27c486d5da8e
Victor Rivas <vctr90w@gmail.com>
2014-08-07 15:12:56 -0430

    Cleaning code

--- public/javascripts/controllers/url_crtl.js
+++ public/javascripts/controllers/url_crtl.js
@@ -293,20 +293,6 @@ angular.module('usys.controllers',[])
 				$scope.url[$scope.url.length-1].jtag = jade_css.first + valid[1] + jade_css.second;
 				$scope.url[$scope.url.length-1].csst = true;
 			}
-			/*$scope.url.push({
-				text:$scope.url.text,
-				id:$scope.url[$scope.url.length-1].id+1,
-				del:false,
-				disabled:false,
-				blank:true,
-				csst:false,
-				jst:false,
-				pkg_name:'',
-				tag:'',
-				jtag:'',
-				url_path:'',
-				remote:null
-			});*/
 			$scope.push_object_element();
 		}
 	}
@@ -341,8 +327,6 @@ angular.module('usys.controllers',[])
 //----------Url validator
 //--We need to check if the url given by a user exists or not
 	$scope.validate = function(){
-	/*	console.log(cdnObject);
-		$scope.as = JSON.parse(cdnObject.content.body);*/
 		$http({method:'POST',url:'/urlGetter',data:{url:$scope.url[$scope.url.length-1].text}})
 				.success(function(data,status,headers,config){
 					console.log('ok');
@@ -378,62 +362,6 @@ angular.module('usys.controllers',[])
 	};
 //------------------------------------------------------------------------------------
 
-/*	$scope.validate = function(){
-		var action = $scope.check();
-		if(action){
-					console.log('ok');
-					var valid = $validator.urlC('ok',$scope.url[$scope.url.length-1].id,$scope.url[$scope.url.length-1].text);
-					if(valid !== false){
-						$scope.url[$scope.url.length-1].disabled = true;
-						$scope.url[$scope.url.length-1].type = valid[0];
-						if(valid[0] === ".js"){
-							$scope.url[$scope.url.length-1].tag = jsT.first + valid[1] + jsT.second;
-						}else{
-							$scope.url[$scope.url.length-1].tag = cssT.first + valid[1] + cssT.second;
-						}
-						$scope.url.push({text:$scope.url.text,id:$scope.url[$scope.url.length-1].id+1,del:false,disabled:false});
-					}
-
-		}else{//console.log($scope.url[$scope.url.length-1].text);
-					console.log('Something is wrong');
-					if($scope.url[$scope.url.length-1].text===undefined){
-						$scope.url = $scope.url.slice(0,$scope.url.length-1);
-					}
-					$validator.urlC('Not found',$scope.url[$scope.url.length-1].id,$scope.url[$scope.url.length-1].text,$validator.eMessage.NotUrl);
-
-		}
-	}
-
-	$scope.check = function(){
-		var key = (+new Date) + "" + Math.random();
-//		console.log($scope.url[$scope.url.length-1].text);
-		$scope.urli = $sce.trustAsResourceUrl($scope.url[$scope.url.length-1].text);
-		
-		$('#ifr')[0].onload = function(){
-			alert('passed');
-		}console.log($('#ifr'));
-			if($('#if').children!==undefined){
-				console.log('asd');
-			}else{console.log('sss');}
-			var global = $('#ifr')[0].contentWindow;
-			global[key] = "asd";
-		if(global[key]==="asd"){
-			return true;
-		}else{
-			return false;
-		}
-		try {
-			var global = $('#ifr')[0].contentWindow;
-			global[key] = "asd";
-			return global[key] === "asd";
-		}
-		catch (e){
-			return false;
-		}
-	};*/
-
-/*	$scope.urli = "sadasant.com/js/Shade.js";
-	$scope.urli = $sce.trustAsResourceUrl($scope.urli);*/
 
 //----------Deletion from url object
 	$scope.delet = function(id){
--- routes/index.js
+++ routes/index.js
@@ -16,22 +16,6 @@ router.post('/urlGetter', function(req,res){
 	request(url,function(error,response,body){
 		if(body!==undefined){res.send(200)}else{res.send(404);}
 	});
-
-/*	var command = 'wget ' + url + ' -O /dev/null';
-	ex = function(command,callback){
-		exec(command,function(error,stdout,stderr){
-			callback(stdout,error,stderr);
-		});
-	};
-
-	ex(command,function(data,error,serror){
-		if(serror.indexOf('200 OK')!==-1){
-			res.send(200);
-		}else{
-			res.send(404);
-		}
-	});*/
-
 });
 //---------------------------------------------------------
 
